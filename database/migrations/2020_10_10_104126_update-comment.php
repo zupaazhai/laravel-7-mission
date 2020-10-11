173d476d24d833305d62343d23758fb9b885e043
Jakkrit Prasert <jakkritt26@gmail.com>
2020-10-11 18:18:25 +0700

    success

---
+++ database/migrations/2020_10_10_104126_update-comment.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateComment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function($table) {
+            $table->text('comment');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
