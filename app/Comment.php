52cd063ac381e0fd852f35d75a11aa8ba74b39c1
tuey30716 <tueygo@gmail.com>
2021-07-09 12:48:56 +0700

    Add files via upload

--- app/Comment.php
+++ app/Comment.php
@@ -6,4 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    public $fillable = ['topic_id','user_id','comment'];
+
+    /**
+     * Relation with table user
+     *
+     * @return App\Comment
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+    public function topic()
+    {
+        return $this->belongsTo('App\Topic', 'topic_id', 'id');
+    }
+
 }
